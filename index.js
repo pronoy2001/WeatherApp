ff86ae036e644fc73e9d24a466790858dacc3bed
pronoy2001 <pronoydutta1996@gmail.com>
2023-08-04 21:45:00 +0530

    Handle user error

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
-function search(){
-const result= document.getElementById("result");
-const h2=document.createElement("h2");
-h2.innerText= "25 Degree";
-result.appendChild(h2);     
+
+
+function search() {
+
+    const data = [
+        {
+            city: "Kolkata",
+            temp: 28,
+            weather: "Stormy"
+        },
+        {
+            city: "Mumbai",
+            temp: 27,
+            weather: "Haze"
+        },
+        {
+            city: "Delhi",
+            temp: 25,
+            weather: "Cloudy"
+        },
+        {
+            city: "Bangalore",
+            temp: 20,
+            weather: "Rainy"
+        },
+    ]
+
+    const searchCity = document.getElementById("search-city").value;
+    const section = document.getElementById("weather-card")
+    if (searchCity != "") {
+        let cityName;
+        let temperature;
+        let weatherType;
+        let found = false;
+        for (let i = 0; i < data.length; i++) {
+            const cityData = data[i];
+            if (cityData.city.toLowerCase() === searchCity.toLowerCase()) {
+                found = true;
+                cityName = cityData.city;
+                temperature = cityData.temp;
+                weatherType = cityData.weather;    
+                section.style.display = "block";
+            }
+        }
+        if(found === false){
+            alert("City not found");
+            section.style.display = "none";
+        }
+        const city = document.getElementById("city");
+        city.innerText = cityName;
+
+        const temp = document.getElementById("temperature");
+        temp.innerText = temperature;
+
+        const weather = document.getElementById("weatherType");
+        weather.innerText = weatherType;
+
+        const today = new Date();
+        const time = document.getElementById("time");
+        time.innerText = today.getHours() + ":" + today.getMinutes()
+    }
+    else {
+        alert("Please enter a city name");
+        section.style.display = "none";
+    }
 }
